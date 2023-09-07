3694c1c40fdfcc624d19232ec8c437a9589c69f9
tom rosen <trrosen@wisc.edu>
2023-09-06 20:44:55 -0500

    Updating project files

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -47,7 +47,7 @@ const Projects = (props) => {
                 ))}
               </Row>
 
-              {!showMore
+              {!numberOfItems
                 && (
                 <Button
                   style={styles.showMoreStyle}
